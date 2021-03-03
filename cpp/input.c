9ff0cbe355424ac99859d1aa4078aaa361425c9d
Penkwe Tjengh <penkwe@MacOS.local>
2021-03-03 13:12:41 +0800

    sync

--- cpp/input.c
+++ cpp/input.c
@@ -3,10 +3,10 @@ using namespace std;
 
 int main(){
 
-# 	string first_name;
+ 	string first_name;
 	int age;
 	
-# # 	cout << "Hello," << first_name << "age (" << age << ")\n";
+	cout << "Hello," << first_name << "age (" << age << ")\n";
 	int a;
 	return 0;
 }
